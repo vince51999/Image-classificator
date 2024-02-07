94cd81d7e4d8c2a3ec09e2011f9c9851c50a6c39
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-07 11:26:22 +0100

    Created main function to manage learning

---
+++ main.py
@@ -0,0 +1,26 @@
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+import TinyImageNetDataset
+import Model
+import Training
+import Testing
+
+BATCH_SIZE = 100 # increase / decrease according to GPU memeory
+NUM_EPOCHS = 30 # number of epochs to train for
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def main():
+    dataset = TinyImageNetDataset.TinyImageNetDataset(BATCH_SIZE)
+    model = Model.get_model(type="resnet50", num_classes=200, wieghts=None)
+    model = model.to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.CrossEntropyLoss()
+    
+    Training.train_loop(dataset.train_dataloader,dataset.val_dataloader, model, optimizer, criterion, DEVICE, NUM_EPOCHS)
+    
+if __name__ == "__main__":
+    main()
