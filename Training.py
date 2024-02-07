bc0cf158684f3b18431f1815c4b26001929c067c
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-07 11:28:49 +0100

    Created validation function

--- Training.py
+++ Training.py
@@ -36,8 +36,30 @@ def __train(trainset, model, optimizer, criterion, device):
     return train_loss_list
 
 
+# Validation function
+@torch.no_grad()
+def __val(valset, model, criterion, device):
+    model.eval()
+    print("Validating")
+    val_loss_list = []
+
+    # initialize tqdm progress bar
+    prog_bar = tqdm(valset, total=len(valset))
+
+    for i, data in enumerate(prog_bar):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # put data to device
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        loss_value = loss.item()
+        val_loss_list.append(loss_value)
+
         # update the loss value beside the progress bar for each iteration
         prog_bar.set_description(desc=f"Loss: {loss_value:.4f}")
 
-    print("Finished Training")
-    return train_loss_list
+    print("Finished validating")
+    return val_loss_list
