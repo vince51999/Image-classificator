ac304f0e8a5d3dfb860c69fdc61f6650d7dc6f5d
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-09 11:13:51 +0100

    Deleted unnecessary rows

--- Training.py
+++ Training.py
@@ -3,19 +3,6 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
-def save_model(epoch, model, optimizer):
-    """
-    Function to save the trained model till current epoch, or whenver called
-    """
-    torch.save(
-        {
-            "epoch": epoch + 1,
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-        },
-        "res/faster_rcnn.pth",
-    )
-
 def createChart(xlabel, ylabel, xdata, ydata, path, dataNames=['chart']):
     plt.xlabel(xlabel, fontsize=10)
     plt.ylabel(ylabel, fontsize=10)
@@ -45,33 +32,7 @@ def train_loop(trainset, valset, model, optimizer, criterion, device, epochs):
         print(f"Epoch #{epoch+1} validation loss: {epoch_val_loss:.3f}")
     return train_losses, valid_losses
 
-# Test function
-@torch.no_grad()
-def test(testset, model, criterion, device):
-    model.eval()
-    print("Testing")
-    test_loss_list = []
 
-    # initialize tqdm progress bar
-    prog_bar = tqdm(testset, total=len(testset))
-
-    for i, data in enumerate(prog_bar):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # put data to device
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-
-        loss_value = loss.item()
-        test_loss_list.append(loss_value)
-
-        # update the loss value beside the progress bar for each iteration
-        prog_bar.set_description(desc=f"Loss: {loss_value:.4f}")
-
-    print("Finished testing")
-    return test_loss_list
 
 # Training function
 def __train(trainset, model, optimizer, criterion, device):
