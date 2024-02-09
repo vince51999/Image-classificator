9bb63d95f496fa205fc1c5552186aa965b497f6f
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-09 11:14:42 +0100

    Updated createChart function with different style of chart

--- Training.py
+++ Training.py
@@ -8,7 +8,15 @@ def createChart(xlabel, ylabel, xdata, ydata, path, dataNames=['chart']):
     plt.ylabel(ylabel, fontsize=10)
 
     for index, name in enumerate(dataNames):
-        plt.scatter(xdata, ydata[index], label=name)
+        plt.plot(
+            xdata,
+            ydata[index],
+            marker="o",
+            linestyle="dashed",
+            linewidth=2,
+            markersize=6,
+            label=name,
+        )
     plt.title(f"Training results", fontsize=12)
     if (dataNames[0] != 'chart'):
         plt.legend()
