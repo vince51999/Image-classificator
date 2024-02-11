99c2aeb780de857bec0916dccd5dee0e65700392
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-11 10:53:07 +0100

    Optimized code
    Self.correct is not necessary because we can use the diagonal of matrix to know the number of correct prediction

--- Model/Statistics.py
+++ Model/Statistics.py
@@ -4,19 +4,18 @@ from typing import List
 
 class Statistics:
     def __init__(self, classes: List[int], sample_class: int):
-        self.correct = 0.0
         self.classes = classes
         self.sample_class = sample_class
         self.conf_matrix = torch.zeros(len(classes), len(classes))
 
     def update(self, preds, labels):
-        self.correct += Statistics.__get_num_correct(preds, labels)
         _, preds = torch.max(preds, 1)
         for t, p in zip(labels.view(-1), preds.view(-1)):
             self.conf_matrix[t.long(), p.long()] += 1
 
     def accuracy(self):
-        return self.correct / (self.sample_class * len(self.classes))
+        # accuracy = (TP + TN) / (TP + TN + FP + FN)
+        return self.conf_matrix.diag().sum() / self.conf_matrix.sum()
 
     def classes_recall(self):
         # recall = TP / (TP + FN) for one class
@@ -28,6 +27,3 @@ class Statistics:
 
     def confusion_matrix(self):
         return self.conf_matrix
-
-    def __get_num_correct(preds, labels):
-        return preds.argmax(dim=1).eq(labels).sum().item()
