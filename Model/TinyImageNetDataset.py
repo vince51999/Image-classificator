6da16d705b73edc31fa088b1ec9794e049f8a3be
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-17 17:39:22 +0100

    Calculated and updated the mean and std of the dataset

--- Model/TinyImageNetDataset.py
+++ Model/TinyImageNetDataset.py
@@ -16,11 +16,12 @@ class TinyImageNetDataset(Dataset):
     """
 
     def __init__(self, train_batch_size, eval_batch_size, classes=None):
+        # We have calculate the mean and std of the dataset
         transform = transforms.Compose(
             [
                 transforms.ToTensor(),
                 transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    mean=[0.497, 0.500, 0.502], std=[0.137, 0.139, 0.144]
                 ),
             ]
         )
