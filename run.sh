92a77d9f272f9d40d570b591e9d72201a285004c
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-02-22 17:23:55 +0100

    Updated basic parameters

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:p100:1
 #SBATCH --qos=gpu
-#SBATCH --time 01:00:00
+#SBATCH --time 01:30:00
 #SBATCH --mem=32gb
 #SBATCH --ntasks-per-node 10
 
@@ -21,16 +21,16 @@ cls=0
 num_classes=10
 num_epochs=100
 eval_batch_size=1
-train_batch_size=9
+train_batch_size=5
 tolerance=8
 min_delta=0.5
 lr=0.001
 momentum=0.0
 weight_decay=0.001
-dropout_rate_bb=0.1
-dropout_rate_fc=0.5
-pretrained=0
-is_test=0
+dropout_rate_bb=0.0
+dropout_rate_fc=0.0
+pretrained=1
+is_test=1
 increases_trainset=0
 
 module load miniconda3
