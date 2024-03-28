e42a905434a3404ac984f07625780dedaabf09ad
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-28 11:17:59 +0100

    Created class criterion to manage criterion and to update classes weights

---
+++ Model/Criterion.py
@@ -0,0 +1,49 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Criterion:
+    def __init__(self, num_classes: int, DEVICE):
+        """
+        Initialize the criterion based on the number of classes.
+
+        Args:
+            num_classes (int): The number of classes in the dataset.
+            DEVICE (_type_): The device where the model is trained.
+        """
+        self.criterion = None
+        self.num_classes = num_classes
+        self.DEVICE = DEVICE
+        if self.num_classes == 1:
+            self.criterion = nn.BCELoss()
+            print(f"Criterion: BCELoss")
+        else:
+            class_weights = torch.tensor([1.0 for _ in range(self.num_classes)])
+            self.criterion = nn.CrossEntropyLoss(weight=class_weights.to(self.DEVICE))
+            print(f"Criterion: CrossEntropyLoss")
+
+    def update_classes_weights(self, conf_matrix: np.ndarray) -> None:
+        """
+        Update the classes weights based on the confusion matrix.
+
+        Args:
+            conf_matrix (np.ndarray): The confusion matrix representing the classification results.
+
+        Returns:
+            None
+        """
+        if self.num_classes == 1:
+            return
+        samples_per_class = conf_matrix.sum(axis=1)
+        diag = np.diag(conf_matrix)
+        diag = np.where(diag == 0, 1, diag)
+        print("Diag: ", diag)
+        # Compute the weight adjustment based on confusion matrix
+        # We use **2 to give more weight to the classes with fewer true positives
+        weights_adjustment = (samples_per_class / diag) ** 2
+        new_weights = weights_adjustment / weights_adjustment.sum()
+        print("Updated classes weights: ", new_weights)
+
+        new_weights = new_weights.to(self.DEVICE)
+        self.criterion.weight = new_weights
