c13fb97405521871007d954e37e34034e116e1cd
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-29 23:57:39 +0100

    Added condition to not apply early stop

--- Model/EarlyStopping.py
+++ Model/EarlyStopping.py
@@ -1,12 +1,16 @@
 class EarlyStopping:
     def __init__(self, tolerance: int = 5, min_delta: float = 0):
-
+        self.apply = True
+        if tolerance < 1 or min_delta <= 0:
+            self.apply = False
         self.tolerance = tolerance
         self.min_delta = min_delta
         self.counter = 0
         self.early_stop = False
 
     def __call__(self, train_loss: float, validation_loss: float):
+        if not self.apply:
+            return
         if (validation_loss - train_loss) > self.min_delta:
             self.counter += 1
             if self.counter >= self.tolerance:
