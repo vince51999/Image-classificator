4d54bf13f672b2089cb38ac079a337db120ca584
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-06-09 14:32:20 +0200

    Updated .bat and .sh files

--- run.bat
+++ run.bat
@@ -1,24 +1,61 @@
 @echo off
-SET architecture=resnet18
-SET cls=0
-SET num_classes=3
-SET num_epochs=3
-SET eval_batch_size=16
-SET train_batch_size=16
-SET gamma_train_batch_size=2
-SET tolerance=6
-SET min_delta=0.05
-SET lr=0.01
-SET gamma_lr=0.2
-SET momentum=0.9
-SET weight_decay=0.0005
-SET dropout_rate_bb=0.3
-SET dropout_rate_fc=0.5
-SET fine_tune=1
-SET transfer_learning=1
-SET is_test=0
-SET increases_trainset=0
-SET image_size=112
-SET step=2
 
-python "%CD%\main.py" "--architecture" "%architecture%" "--cls" "%cls%" "--num_classes" "%num_classes%" "--num_epochs" "%num_epochs%" "--eval_batch_size" "%eval_batch_size%" "--train_batch_size" "%train_batch_size%" "--gamma_train_batch_size" "%gamma_train_batch_size%" "--tolerance" "%tolerance%" "--min_delta" "%min_delta%" "--lr" "%lr%" "--gamma_lr" "%gamma_lr%" "--momentum" "%momentum%" "--weight_decay" "%weight_decay%" "--dropout_rate_bb" "%dropout_rate_bb%" "--dropout_rate_fc" "%dropout_rate_fc%" "--fine_tune" "%fine_tune%" "--transfer_learning" "%transfer_learning%" "--test" "%is_test%" "--increases_trainset" "%increases_trainset%" "--image_size" "%image_size%" "--step" "%step%"
+REM architecture of the model
+REM resnet18, resnet50, resnet101
+SET architecture=resnet18
+REM if fine_tune=0, transfer_learning=0, the model is trained from scratch
+REM if fine_tune=1, transfer_learning=1, the model is fine-tuned
+SET fine_tune=0
+SET transfer_learning=0
+REM position of dropout layer in the residual block
+REM 0: after each residual block
+REM 1: before each ReLU
+REM 2: after each ReLU
+SET dropout_pos_rb=0
+REM dropout rate of dropout layer in the residual block, 0.0 means no dropout
+SET dropout_rate_rb=0.5
+REM position of dropout layer in the head
+REM 0: before avgpool
+REM 1: after avgpool
+SET dropout_pos_fc=0
+REM dropout rate of dropout layer in the head, 0.0 means no dropout
+SET dropout_rate_fc=0.7
+
+REM cls is the class that we want in the classification task
+REM test is the flag to test the model on a pre-defined classes (10 classes from Tiny ImageNet dataset)
+SET cls=0
+SET num_classes=2
+SET is_test=0
+
+REM parameters for early stopping
+REM tolerance minimum value 1
+REM min_delta minimum value 0.0
+SET tolerance=2
+SET min_delta=0.05
+
+REM parameters for training, if momentum=0.0, the optimizer is Adam else the optimizer is SGD
+SET num_epochs=10
+SET lr=0.001
+SET eval_batch_size=4
+SET train_batch_size=16
+SET momentum=0.0
+SET weight_decay=0.0005
+
+REM parameters for schedulers
+SET gamma_lr=0.1
+SET gamma_train_batch_size=2
+SET step=2
+REM lr_scheduler: StepLR, CosineAnnealingWarmRestarts
+SET lr_scheduler="CosineAnnealingWarmRestarts"
+
+REM parameters for data augmentation, the minimum size of the image is 64x64
+SET increases_trainset=0
+SET image_size=64
+
+REM load the model from the checkpoint
+REM checkpoint is the path to the checkpoint
+REM none: train the model from scratch
+SET checkpoint="none"
+
+
+python "%CD%\main.py" "--architecture" "%architecture%" "--cls" "%cls%" "--num_classes" "%num_classes%" "--num_epochs" "%num_epochs%" "--eval_batch_size" "%eval_batch_size%" "--train_batch_size" "%train_batch_size%" "--gamma_train_batch_size" "%gamma_train_batch_size%" "--tolerance" "%tolerance%" "--min_delta" "%min_delta%" "--lr" "%lr%" "--gamma_lr" "%gamma_lr%" "--momentum" "%momentum%" "--weight_decay" "%weight_decay%" "--dropout_rate_rb" "%dropout_rate_rb%" "--dropout_rate_fc" "%dropout_rate_fc%" "--fine_tune" "%fine_tune%" "--transfer_learning" "%transfer_learning%" "--test" "%is_test%" "--increases_trainset" "%increases_trainset%" "--image_size" "%image_size%" "--step" "%step%" "--dropout_pos_rb" "%dropout_pos_rb%" "--dropout_pos_fc" "%dropout_pos_fc%" "--checkpoint" "%checkpoint%" "--lr_scheduler" "%lr_scheduler%"
--- run.sh
+++ run.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
-architecture="resnet18"
-cls=(0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.0)
-num_classes=10
-num_epochs=100
-eval_batch_size=128
-train_batch_size=128
-gamma_train_batch_size=2
-tolerance=8
-min_delta=0.5
-lr=0.001
-gamma_lr=0.3
-momentum=0.9
-weight_decay=0.00001
-dropout_rate_bb=0.1
-dropout_rate_fc=0.2
-fine_tune=0
-transfer_learning=1
-is_test=1
-increases_trainset=3
-image_size=112
-step=0
 
-python ./main.py --architecture $architecture --cls $cls --num_classes $num_classes --num_epochs $num_epochs --eval_batch_size $eval_batch_size --train_batch_size $train_batch_size --gamma_train_batch_size $gamma_train_batch_size --tolerance $tolerance --min_delta $min_delta --lr $lr --gamma_lr $gamma_lr --momentum $momentum --weight_decay $weight_decay --dropout_rate_bb $dropout_rate_bb --dropout_rate_fc $dropout_rate_fc --fine_tune $fine_tune --transfer_learning $transfer_learning --test $is_test --increases_trainset $increases_trainset --image_size $image_size --step $step
+# Architecture of the model
+# resnet18, resnet50, resnet101
+architecture="resnet18"
+# If fine_tune=0, transfer_learning=0, the model is trained from scratch
+# If fine_tune=1, transfer_learning=1, the model is fine-tuned
+fine_tune=0
+transfer_learning=0
+# Position of dropout layer in the residual block
+# 0: after each residual block
+# 1: before each ReLU
+# 2: after each ReLU
+dropout_pos_rb=0
+# Dropout rate of dropout layer in the residual block, 0.0 means no dropout
+dropout_rate_rb=0.5
+# Position of dropout layer in the head
+# 0: before avgpool
+# 1: after avgpool
+dropout_pos_fc=0
+# Dropout rate of dropout layer in the head, 0.0 means no dropout
+dropout_rate_fc=0.7
+
+# cls is the class that we want in the classification task
+# test is the flag to test the model on a predefined classes (10 classes from Tiny ImageNet dataset)
+cls=0
+num_classes=2
+is_test=0
+
+# Parameters for early stopping
+# tolerance minimum value 1
+# min_delta minimum value 0.0
+tolerance=2
+min_delta=0.05
+
+# Parameters for training, if momentum=0.0, the optimizer is Adam else the optimizer is SGD
+num_epochs=10
+lr=0.001
+eval_batch_size=4
+train_batch_size=16
+momentum=0.0
+weight_decay=0.0005
+
+# Parameters for schedulers
+gamma_lr=0.1
+gamma_train_batch_size=2
+step=2
+# lr_scheduler: StepLR, CosineAnnealingWarmRestarts
+lr_scheduler="CosineAnnealingWarmRestarts"
+
+# Parameters for data augmentation, the minimum size of the image is 64x64
+increases_trainset=0
+image_size=64
+
+# Load the model from the checkpoint
+# checkpoint is the path to the checkpoint
+# none: train the model from scratch
+checkpoint="none"
+
+python ./main.py --architecture "$architecture" --cls "$cls" --num_classes "$num_classes" --num_epochs "$num_epochs" --eval_batch_size "$eval_batch_size" --train_batch_size "$train_batch_size" --gamma_train_batch_size "$gamma_train_batch_size" --tolerance "$tolerance" --min_delta "$min_delta" --lr "$lr" --gamma_lr "$gamma_lr" --momentum "$momentum" --weight_decay "$weight_decay" --dropout_rate_rb "$dropout_rate_rb" --dropout_rate_fc "$dropout_rate_fc" --fine_tune "$fine_tune" --transfer_learning "$transfer_learning" --test "$is_test" --increases_trainset "$increases_trainset" --image_size "$image_size" --step "$step" --dropout_pos_rb "$dropout_pos_rb" --dropout_pos_fc "$dropout_pos_fc" --checkpoint "$checkpoint" --lr_scheduler "$lr_scheduler"
